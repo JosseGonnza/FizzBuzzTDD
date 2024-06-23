e39be20b5103e2601783ef75169d38f9a16c271d
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-23 18:52:48 +0100

    FizzBuzz Complete!

--- src/test/java/com/jossegonnza/FizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/jossegonnza/FizzBuzz/FizzBuzzTest.java
@@ -13,4 +13,31 @@ class FizzBuzzTest {
         Assertions.assertThat(fizzBuzz.calculate(6)).isEqualTo("Fizz");
         Assertions.assertThat(fizzBuzz.calculate(9)).isEqualTo("Fizz");
     }
+
+    @Test
+    void multiple_of_five_it_will_return_the_word_Buzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Assertions.assertThat(fizzBuzz.calculate(5)).isEqualTo("Buzz");
+        Assertions.assertThat(fizzBuzz.calculate(10)).isEqualTo("Buzz");
+        Assertions.assertThat(fizzBuzz.calculate(20)).isEqualTo("Buzz");
+    }
+
+    @Test
+    void multiple_of_three_and_five_it_will_return_the_word_FizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Assertions.assertThat(fizzBuzz.calculate(15)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.calculate(30)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.calculate(45)).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    void if_not_multiple_of_three_and_five_it_will_return_the_number() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Assertions.assertThat(fizzBuzz.calculate(1)).isEqualTo("1");
+        Assertions.assertThat(fizzBuzz.calculate(2)).isEqualTo("2");
+        Assertions.assertThat(fizzBuzz.calculate(4)).isEqualTo("4");
+    }
 }
