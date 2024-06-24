29006f2e333675680e4c8e093db6346261890bd1
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-24 11:50:52 +0100

    🟢 If there is a number 3 duplicate Fizz

--- src/main/java/com/jossegonnza/FizzBuzz/FizzBuzz.java
+++ src/main/java/com/jossegonnza/FizzBuzz/FizzBuzz.java
@@ -2,17 +2,27 @@ package com.jossegonnza.FizzBuzz;
 
 public class FizzBuzz {
 
+
     public String calculate(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        }
+    StringBuilder result = new StringBuilder();
+
         if (number % 3 == 0) {
-            return "Fizz";
+            result.append("Fizz");
         }
         if (number % 5 == 0) {
-            return "Buzz";
+            result.append("Buzz");
         }
+        if (containsNum3(number)) {
+            result.append("Fizz");
+        }
+        if (result.length() > 0) {
+            return result.toString();
+        }
+
         return String.valueOf(number);
     }
 
+    private Boolean containsNum3(int number) {
+        return String.valueOf(number).contains("3");
+    }
 }
--- src/test/java/com/jossegonnza/FizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/jossegonnza/FizzBuzz/FizzBuzzTest.java
@@ -9,27 +9,25 @@ class FizzBuzzTest {
     void multiple_of_three_it_will_return_the_word_Fizz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        Assertions.assertThat(fizzBuzz.calculate(3)).isEqualTo("Fizz");
         Assertions.assertThat(fizzBuzz.calculate(6)).isEqualTo("Fizz");
         Assertions.assertThat(fizzBuzz.calculate(9)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz.calculate(12)).isEqualTo("Fizz");
     }
 
     @Test
     void multiple_of_five_it_will_return_the_word_Buzz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        Assertions.assertThat(fizzBuzz.calculate(5)).isEqualTo("Buzz");
         Assertions.assertThat(fizzBuzz.calculate(10)).isEqualTo("Buzz");
         Assertions.assertThat(fizzBuzz.calculate(20)).isEqualTo("Buzz");
+        Assertions.assertThat(fizzBuzz.calculate(40)).isEqualTo("Buzz");
     }
 
     @Test
     void multiple_of_three_and_five_it_will_return_the_word_FizzBuzz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        Assertions.assertThat(fizzBuzz.calculate(15)).isEqualTo("FizzBuzz");
-        Assertions.assertThat(fizzBuzz.calculate(30)).isEqualTo("FizzBuzz");
-        Assertions.assertThat(fizzBuzz.calculate(45)).isEqualTo("FizzBuzz");
+        Assertions.assertThat(fizzBuzz.calculate(60)).isEqualTo("FizzBuzz");
     }
 
     @Test
@@ -40,4 +38,14 @@ class FizzBuzzTest {
         Assertions.assertThat(fizzBuzz.calculate(2)).isEqualTo("2");
         Assertions.assertThat(fizzBuzz.calculate(4)).isEqualTo("4");
     }
+
+    @Test
+    void if_there_is_a_number_3_duplicate_Fizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Assertions.assertThat(fizzBuzz.calculate(3)).isEqualTo("FizzFizz");
+        Assertions.assertThat(fizzBuzz.calculate(13)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz.calculate(23)).isEqualTo("Fizz");
+    }
+
 }
